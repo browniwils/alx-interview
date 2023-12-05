e95936c5d2aa1c73280ece8abb4cfb3429060563
browniwils <browniwils@gmail.com>
2023-12-05 17:44:11 +0000

    completed project

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/bin/usr/env python3
+"""Module for initiating pascal triangle."""
+
+
+def pascal_triangle(n):
+    """Returns a list of lists of integers representing 
+    the Pascal’s triangle."""
+    if n <= 0:
+        return []
+
+    pascal = []
+    for i in range(n):
+        row = [1] * (i + 1)
+        if i >= 2:
+            for j in range(1, i):
+                row[j] = pascal[i - 1][j - 1] + pascal[i - 1][j]
+        pascal.append(row)
+
+    return pascal
---
+++ README.md
@@ -0,0 +1,1 @@
+### ALX Interview project repository 
