0388bf796aca5e77701e5edb407eb8189645d46e
browniwils <browniwils@gmail.com>
2023-12-27 01:28:34 +0000

    completed task 0x02

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Minimum operation module."""
+
+def minOperations(n: int) -> int:
+    """
+    Determine the minimum opeartions for running
+    Args:
+        n (int):
+    Returns:
+        Returns an int
+    """
+    if not isinstance(n, int):
+        raise TypeError('Expected an integer')
+    if n < 2:
+        return 0
+    operations, root = 0, 2
+    while root <= n:
+        if n % root == 0:
+            operations += root
+            n = n / root
+            root -= 1
+        root += 1
+    return operations
