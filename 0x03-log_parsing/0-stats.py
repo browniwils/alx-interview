7b27cb3e38ab05eae1a7e8f0b4060900a1cc5068
browniwils <browniwils@gmail.com>
2024-01-10 18:17:21 +0000

    fix run in code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-"""Module for parsing log usefull information and print it to standard output."""
+"""Module for parsing log information and present it to standard output.
+"""
 
-import sys
 import re
 
 
-def check_log_format(log_string):
-    regex_pattern = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
-    return re.match(regex_pattern, log_string)
+def valid_log(log_string):
+    """Validate log inputs.
+    """
+    patt = r'^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[.*\] "GET \/projects\/260 HTTP\/1\.1" \d{3} \d+'
+    return re.match(patt, log_string)
 
-def print_to_stdout(**kwargs):
-    """Prints to standard output."""
+def display_log(**kwargs):
+    """Prints infofmation to standard output.
+    """
     for key in kwargs.keys():
         if not isinstance(kwargs.get(key), dict):
             print("File Size {}".format(kwargs.get(key)))
@@ -18,8 +21,9 @@ def print_to_stdout(**kwargs):
             for nested_k, nested_v in key.items():
                 print(f"{nested_k}: {nested_v}")
 
-def read_and_analyze_log_files():
-    """Log analyzer."""
+def log_task():
+    """Log parser.
+    """
     while True:
         try:
             status_codes = {
@@ -33,16 +37,20 @@ def read_and_analyze_log_files():
                 "500": 0
 
             }
-            total_size = 0
+            size = 0
             for time in range(11):
-                log_info = input("").strip()
-                if check_log_format(log_info):
-                    log_info_list = log_info.split(" ")
-                    f_size = log_info_list[-1]
-                    s_code = log_info_list[-2]
+                info = input("").strip()
+                if valid_log(info):
+                    info_list = info.split(" ")
+                    fize_size = info_list[-1]
+                    s_code = info_list[-2]
                     status_codes[s_code] = status_codes.get(s_code) + 1
-                    total_size += int(f_size)
+                    size += int(fize_size)
                     sorted_keys_dict = {code: status_codes[code] for code in sorted(status_codes)}
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
+            display_log(codes=sorted_keys_dict, size = size)
         except KeyboardInterrupt:
-            print_to_stdout(codes=sorted_keys_dict, size = total_size)
+            display_log(codes=sorted_keys_dict, size = size)
+
+
+if "__main__" == __name__:
+    log_task()
