1785b7e6d89af84dbb66f1a01a57776ac26ad4f5
browniwils <browniwils@gmail.com>
2024-02-06 15:01:42 +0000

    Complete project 0x09

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -17,11 +17,11 @@ def island_perimeter(grid):
             if cell == 0:
                 continue
             edges = (
-                i == 0 or (len(grid[i - 1]) > j 
+                i == 0 or (len(grid[i - 1]) > j \
                            and grid[i - 1][j] == 0),
-                j == m - 1 or (m > j + 1 
+                j == m - 1 or (m > j + 1 \
                                and row[j + 1] == 0),
-                i == n - 1 or (len(grid[i + 1]) > j 
+                i == n - 1 or (len(grid[i + 1]) > j \
                                and grid[i + 1][j] == 0),
                 j == 0 or row[j - 1] == 0,
             )
