7ce80de3d6f229608363022268405705f1c20295
browniwils <browniwils@gmail.com>
2024-02-15 13:27:16 +0000

    Complete project 0x0A

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,40 @@
+#!/usr/bin/pytho3
+
+"""Module for prime game."""
+
+def isWinner(x, nums):
+    """Determines a winner in the prime game."""
+
+    if (x < 1):
+        return None
+
+    if (nums is None):
+        return None
+
+    player_one = {"name": "Maria", "won": 0}
+    player_two = {"name": "Ben", "won": 0}
+
+    max_num = max(nums)
+    all_num = [True for x in range(1, max_num + 1)]
+    all_num[0] = False
+
+    for idx, prime_num in enumerate(all_num, 1):
+        if idx == 1:
+            continue
+        if prime_num is None:
+            continue
+        for i in range(idx + idx, max_num + 1, idx):
+            all_num[i - 1] = False
+
+    for i, j in zip(range(x), nums):
+        num_of_primes = len(list(filter(lambda x: x, all_num[0:max_num])))
+        player_two["won"] += num_of_primes % 2 == 0
+        player_one["won"] += num_of_primes % 2 == 1
+
+    if player_one["won"] == player_two["won"]:
+        return None
+    if player_one["won"] > player_two["won"]:
+        wins = player_one["name"]
+    else: 
+        wins = player_two["name"]
+    return wins
